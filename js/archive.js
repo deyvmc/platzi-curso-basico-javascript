a0d03b5cfe42d85df204417ea2802a9b65cd5c99
nicolas <nicolas.segovia@alumnos.enac.cl>
2022-05-30 23:01:26 -0400

    modificacion suave

---
+++ js/archive.js
@@ -0,0 +1,8 @@
+const ladoCuadrado = 5;
+console.log("lados del cuadrado miden "+ ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4; 
+console.log("El perimetro de mi cuadrado es "+ perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área de mi cuadrado es "+ areaCuadrado + "cm²");
