3aee0b83d24675a7b9a458c454589f290e45b528
nicolas <nicolas.segovia@alumnos.enac.cl>
2022-06-13 23:54:04 -0400

    hice un formulario con precio y descuento, cree una funcion para calcular el descuento y mandar un texto con tal descuento

--- js/descuentos.js
+++ js/descuentos.js
@@ -3,3 +3,16 @@ const porcentajePrecioconDescuento = 100 - descuento;
 const precioFinal = (precio * porcentajePrecioconDescuento) / 100;
 return precioFinal;
 }
+function onClickdiscount(){
+    const inputprice = document.getElementById("priceinput")
+    const pricevalue = priceinput.value;
+
+
+    const inputdiscount = document.getElementById("inputdiscount")
+    const discountvalue = inputdiscount.value;
+
+    const precioConDescuentos = precioConDescuento(pricevalue, discountvalue);
+
+    const resultP = document.getElementById("resultPrice");
+    resultP.innerText = "el precio final es $" + precioConDescuentos;  
+}
